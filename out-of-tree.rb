e4170dbbeef4cfafcbba800b63be654625afe730
Mikhail Klementev <blame@dumpstack.io>
2019-12-26 21:47:05 +0000

    Add out-of-tree formula

---
+++ out-of-tree.rb
@@ -0,0 +1,36 @@
+require "language/go"
+
+class Docker < Requirement
+  fatal true
+
+  satisfy(:build_env => false) { which("docker") }
+
+  def message; <<~EOS
+    docker is required; install it via:
+      brew cask install docker
+    EOS
+  end
+end
+
+class OutOfTree < Formula
+  desc "out-of-tree kernel {module, exploit} development tool"
+  homepage "out-of-tree.io"
+  url "https://code.dumpstack.io/tools/out-of-tree.git", :tag => "v1.2.1"
+
+  depends_on "go" => :build
+
+  depends_on "qemu"
+  depends_on Docker
+
+  def install
+    ENV["GOPATH"] = buildpath
+
+    bin_path = buildpath/"src/code.dumpstack.io/tools/out-of-tree"
+    bin_path.install Dir["*"]
+
+    Language::Go.stage_deps resources, buildpath/"src"
+    cd bin_path do
+      system "go", "build", "-o", bin/"out-of-tree", "."
+    end
+  end
+end
