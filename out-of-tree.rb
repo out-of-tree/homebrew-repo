cfe7480972f4c46be5a080879a183eb0398032b6
Mikhail Klementev <blame@dumpstack.io>
2020-06-15 02:03:10 +0200

    Update version

--- out-of-tree.rb
+++ out-of-tree.rb
@@ -18,7 +18,7 @@ end
 class OutOfTree < Formula
   desc "out-of-tree kernel {module, exploit} development tool"
   homepage "out-of-tree.io"
-  url "https://code.dumpstack.io/tools/out-of-tree.git", :tag => "v1.2.1"
+  url "https://code.dumpstack.io/tools/out-of-tree.git", :tag => "v1.4.0"
 
   depends_on "go" => :build
 
